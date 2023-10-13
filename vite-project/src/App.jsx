991d400542e468e842adcaa821b5502e0dec5b3f
CodeSandbox <support@codesandbox.io>
2023-10-13 15:14:23 +0000

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,106 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // useRef hook
+  const passwordRef = useRef(null);
+
+  const CopyPassswordtoClip = useCallback(() => {
+    console.log("INside copy", passwordRef);
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 9);
+
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) {
+      string += "1234567890";
+    }
+    if (charAllowed) {
+      string += "!@#$%^&*()";
+    }
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * string.length + 1);
+      pass += string.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, charAllowed, numberAllowed, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  // passwordGenerator();
+  return (
+    <>
+      <h1 className="text-white text-center">Password generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800">
+        <div className="flex shadow rounded-lg overflow-hidden mb-4  ">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+            onClick={CopyPassswordtoClip}
+          >
+            {" "}
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-l">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+
+            <label htmlFor="numberInput"> Numbers </label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="charInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+
+            <label htmlFor="charInput"> Characters </label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
